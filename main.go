bc5bc88ba978f614d4f7368219551dc80e786e52
djshow832 <zhangming@pingcap.com>
2024-08-19 16:36:58 +0800

    add statusPort back

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net/http"
 	_ "net/http/pprof"
 	"runtime"
 	"strings"
@@ -10,20 +12,28 @@ import (
 	"github.com/djshow832/gnet-proxy/netpoll"
 	"github.com/djshow832/gnet-proxy/poolcli"
 	"github.com/djshow832/gnet-proxy/srvcli"
+	"github.com/djshow832/gnet-proxy/util"
 )
 
 func main() {
 	runtime.GOMAXPROCS(1)
 
 	var port int
+	var statusPort int
 	var mode int
 	var backends string
 	flag.IntVar(&port, "port", 6000, "server port")
+	flag.IntVar(&statusPort, "statusPort", 3080, "status port")
 	flag.IntVar(&mode, "mode", 0, "run mode")
 	flag.StringVar(&backends, "backends", ":4000", "backend addrs")
 	flag.Parse()
 
 	bs := strings.Split(backends, ",")
+
+	// /debug/pprof
+	go func() {
+		util.Try(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", statusPort), nil))
+	}()
 
 	switch mode {
 	case 0:
